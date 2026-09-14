6c1217983984e9694f9f73bf2e3305518f4fc02a
dogwithwings22 <brunogonzaga92@gmail.com>
2026-09-14 11:09:40 +0100

    Create nfl.js

---
+++ api/nfl.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const { week = 1, season = 2026 } = req.query;
+
+  try {
+    const url = `https://www.espn.com/apis/site/v2/sports/football/nfl/scoreboard?week=${week}&season=${season}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
